7171146ccf7c82840544a134d45ac7f62b29a7cf
Andrew-Bray <amichaelbray@gmail.com>
2020-11-13 15:46:28 -0800

    make App.js a router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import USMapChart from './USMapChart.js';
+import Header from './Header.js';
+import Footer from './Footer.js';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div className = "body">
-        <USMapChart/>
+      <div className="body">
+        <Router>
+          <Header />
+          <Switch>
+            <Route
+              path="/mapchart"
+              exact
+              render={(routerProps) => <USMapChart {...routerProps} />}
+            />
+          </Switch>
+          <Footer />
+        </Router>
       </div>
     )
   }
