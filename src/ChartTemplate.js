8ea0cd4c7c4a36a687aa22caa8c354ca932cd49b
Katie <katieberrym@gmail.com>
2020-11-16 15:39:10 -0800

    imported navigation into chart

--- src/ChartTemplate.js
+++ src/ChartTemplate.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navigation from './Navigation.js'
 
 export default class ChartTemplate extends Component {
   render() {
