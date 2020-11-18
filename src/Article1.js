a4dcc63322c7dfca55ec59e013e14329b1f2c2d4
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-11-17 21:52:03 -0800

    added new article template and worked on home a little also made new article1 css

---
+++ src/Article1.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Article1.css';
+
+export default class Article extends Component {
+    render() {
+        return (
+            <>
+                <div className="container-fun">
+
+                    <div className="gallery">
+
+                        <article className="art1 border">
+                            <img src="plastic-catherine-sheila.jpg" className="gallery__img" alt="Image 1" />
+                            <span className="floater">
+                                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Turpis nunc eget lorem dolor sed viverra ipsum nunc. Tortor dignissim convallis aenean et tortor. Sed viverra tellus in hac. Consectetur a erat nam at lectus urna. Tellus elementum sagittis vitae et leo duis. Est placerat in egestas erat imperdiet. Pharetra massa massa ultricies mi quis hendrerit dolor. Et tortor consequat id porta.
+                            </span>
+                        </article>
+
+                        <article className="art2 border">
+                            <img src="dump-tom-fisk.jpg" className="gallery__img" alt="Image 2" />
+                            <span className="floater">
+                                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Turpis nunc eget lorem dolor sed viverra ipsum nunc. 
+                            </span>   
+                        </article>
+
+                        <article className="art3 border">
+                            <img src="trees.png" className="gallery__img" alt="Image 3" />
+                            <span className="floater">
+                                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ullamcorp
+                            </span>
+                        </article>
+
+                        <article className="art4 border">
+                            <img src="fire.jpg" className="gallery__img" alt="Image 4" />
+                            <span className="floater">
+                                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Turpis nunc eget lorem dolor sed viverra ipsum nunc. Tortor dignissim convallis aenean et tortor. Sed viverra tellus in hac. Consectetur a erat nam at lectus urna. Tellus elementum sagittis vitae et leo duis. Est placerat in egestas erat imperdiet. 
+                            </span>
+                        </article>
+
+                    </div>
+
+                </div>
+            </>
+        )
+    }
+}
