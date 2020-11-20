ef2f034273a3b1f32043a316956d11cf26a1d9ad
Andrew-Bray <amichaelbray@gmail.com>
2020-11-19 22:25:12 -0800

    working favorited articles wow

--- src/LinkItem.js
+++ src/LinkItem.js
@@ -10,9 +10,8 @@ export default class LinkItem extends Component {
   }
   //ON COMPONENT LOAD...
   componentDidMount = async () => {
-    console.log('fetching favorites');
     if (localStorage.getItem('TOKEN')) await this.fetchFavorites();
-    console.log('fetched favorites');
+
 
   }
 
@@ -21,27 +20,25 @@ export default class LinkItem extends Component {
     const response = await request
       .get(`https://serene-temple-06405.herokuapp.com/api/fav_url`)
       .set('Authorization', localStorage.getItem('TOKEN'))
-
-    //this.setState({ fav_url: response.body.fav_url })
-    return response.body;
+    this.setState({ fav_url: response.body })
 
   }
 
-  handleFavorite = async (fav_url) => {
+  handleFavorite = async (booger) => {
     const favorite = {
-      fav_url: fav_url
+      fav_url: booger
     };
     console.log(favorite);
-    // await request
-    //   .post(`https://serene-temple-06405.herokuapp.com/api/fav_url`)
-    //   .set('Authorization', localStorage.getItem('TOKEN'))
-    //   .send(favorite);
+    await request
+      .post(`https://serene-temple-06405.herokuapp.com/api/fav_url`)
+      .set('Authorization', localStorage.getItem('TOKEN'))
+      .send(favorite);
 
-    // await this.fetchFavorites();
+    await this.fetchFavorites();
   }
 
   render() {
-    console.log(this.fetchFavorites());
+    console.log(this.state.fav_url);
     return (
       <div className="my-tiny-link-wrapper">
         <ReactTinyLink className="tinylink"
@@ -50,7 +47,7 @@ export default class LinkItem extends Component {
           maxLine={4}
           minLine={2}
           width={"45vw"}
-          //proxyUrl="https://alchemy-anywhere.herokuapp.com/"
+          proxyUrl="https://alchemy-anywhere.herokuapp.com/"
           url={this.props.url}
         />
         {
