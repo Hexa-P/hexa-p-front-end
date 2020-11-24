24d9c6ef9658d8a5b764da773f4189ad55ebf766
sformichella <sformichella@gmail.com>
2020-11-23 16:30:39 -0800

    remove logarithmic thing

--- src/ChartTemplate.js
+++ src/ChartTemplate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
-import regression, { logarithmic } from 'regression';
+import regression from 'regression';
 import moment from 'moment';
 import Navigation from './Navigation.js';
 import Header from './Header.js';
